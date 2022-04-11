c9959b8bdbfecfe96a7cb35ce905a5d39f37387d
Dan Hemerlein <dan@halfhelix.com>
2022-04-11 18:10:55 -0400

    add pages

---
+++ pages/software.js
@@ -0,0 +1,47 @@
+import React, { useState } from 'react';
+
+import { FlexContainer, P } from 'react-yan';
+import styled from 'styled-components';
+import { Tabs, Tab } from '@mui/material';
+
+import musicData from './data/music';
+
+const Paragraph = styled(P)`
+  color: ${({ theme }) => {
+    return theme.light.dark;
+  }};
+`;
+
+const Container = styled(FlexContainer)`
+  max-width: 500px;
+`;
+
+const StyledTab = styled(Tab)`
+  color: ${({ theme }) => {
+    return theme.light.dark;
+  }};
+`;
+
+const TabPanel = styled.div`
+  color: ${({ theme }) => {
+    return theme.light.dark;
+  }};
+`;
+
+const Software = () => {
+  const { data } = musicData;
+
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setSelectedTab(newValue);
+  };
+
+  return (
+    <div>
+      <Paragraph>software</Paragraph>
+    </div>
+  );
+};
+
+export default Software;

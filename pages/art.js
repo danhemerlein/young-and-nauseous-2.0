4244eb267992399e5334a21d2ac4d011969bf9cb
Dan Hemerlein <dan@halfhelix.com>
2022-04-19 00:11:51 -0400

    create credits page

--- pages/art.js
+++ pages/art.js
@@ -1,12 +1,30 @@
-import { Paragraph } from 'styles/elements/typography';
+import styled from 'styled-components';
+
+import { HeadlineOne } from 'styles/elements/typography';
+import Heart from 'components/assets/svg/Heart';
 import Footer from 'components/Footer/Footer';
+import { FlexContainer } from 'react-yan';
+
+const Container = styled(FlexContainer)`
+  height: calc(100vh - 20px);
+  overflow: scroll;
+`;
+
+const Inner = styled(FlexContainer)`
+  height: 100%;
+`;
 
 const Art = () => {
   return (
-    <div>
-      <Paragraph>art</Paragraph>
+    <Container direction="column">
+      <Inner direction="column" justify="space-between">
+        <main>
+          <HeadlineOne>art</HeadlineOne>
+          <Heart width="140" height="140" fill="#C23B22" />
+        </main>
+      </Inner>
       <Footer />
-    </div>
+    </Container>
   );
 };
 
